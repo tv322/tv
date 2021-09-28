85fdc7a49a536b8d825dfd635ce46a2046a3b245
tv322 <91540386+tv322@users.noreply.github.com>
2021-09-28 14:44:29 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let potentiometer = 0
 basic.forever(function () {
-	
+    potentiometer = pins.analogReadPin(AnalogPin.P1)
+    motors.dualMotorPower(Motor.A, pins.map(
+    potentiometer,
+    0,
+    1023,
+    0,
+    100
+    ))
 })
